00873b054c5e6b508958fa7f7c9612b1641c11ed
Steve Rancour <18683379+srancour@users.noreply.github.com>
2026-01-30 12:27:27 -0500

    feat: add function to create custom grid

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ const grid = document.getElementById("grid");
 let numberRows = 16;
 let numberColumns = 16;
 
+let rows = document.querySelectorAll(".row");
+let cells = document.querySelectorAll(".column");
+
 function makeGrid (rows, columns) {
     log(rows, columns);
     for (i = 0; i < rows; i++) {
@@ -20,14 +23,70 @@ function makeGrid (rows, columns) {
             makeRow.appendChild(makeColumn);
         }
     }
+    rows = document.querySelectorAll(".row");
+    cells = document.querySelectorAll(".column");
+    cells.forEach(cell => {
+        cell.addEventListener("mouseover", (event) => {
+            cell.classList.add("filled");
+        });
+    });
 }
 
 makeGrid(numberRows, numberColumns);
 
-const cells = document.querySelectorAll(".column");
+const selectButton = document.getElementById("select");
 
-cells.forEach(element => {
-    element.addEventListener("mouseover", (event) => {
-        element.classList.add("filled");
+selectButton.addEventListener("click", (event) =>{
+    let height;
+    let width;
+    function setWidth(){
+        width = prompt("Set width of grid");
+
+        if (width !== null && width !== "") {
+            width = Number(width);
+            if (Number.isInteger(width)){
+                if (width <= 100 && width >= 1) {
+                    return;
+                } else {
+                    alert("You must choose a number between 1 and 100");
+                    setWidth();
+                }
+            } else {
+                alert("You must put in a number");
+                setWidth();
+            }
+        } else {
+            alert("You must set a width for the grid");
+            setWidth();
+        }
+    }
+
+    function setHeight(){
+        height = prompt("Set height of grid");
+
+        if (height !== null && height !== "") {
+            height = Number(height);
+            if (Number.isInteger(height)){
+                if (height <= 100 && height >= 1) {
+                    return;
+                } else {
+                    alert("You must choose a number between 1 and 100");
+                    setHeight();
+                }
+            } else {
+                alert("You must put in a number");
+                setHeight();
+            }
+        } else {
+            alert("You must set a height for the grid");
+            setHeight();
+        }
+    }
+    setWidth();
+    setHeight();
+    rows = document.querySelectorAll(".row");
+    rows.forEach(row => {
+        row.remove();
     });
+    makeGrid(height, width);
 });
